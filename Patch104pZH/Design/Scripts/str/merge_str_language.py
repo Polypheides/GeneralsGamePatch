a49dd46488e4b582a1f5c80ce4a7da1f0356b867
Polypheides <rivaljohansson@gmail.com>
2025-03-13 20:22:18 +0100

    adding generals_str_upgrade for SE

--- Patch104pZH/Design/Scripts/str/merge_str_language.py
+++ Patch104pZH/Design/Scripts/str/merge_str_language.py
@@ -16,6 +16,7 @@ def run():
     #generals_str_upgrade = build_abs_path("data/ukrainian_zh_by_yarpik/generals.str")
     #generals_str_upgrade = build_abs_path("data/ukrainian_zh_by_yarpik_windstalker_edit_1/generals.str")
     generals_str_upgrade = build_abs_path("data/ukrainian_zh_by_yarpik_windstalker_edit_2/generals.str")
+    generals_str_upgrade = build_abs_path("data/Swedish/Swedish.str")
     generals_str_new = build_abs_path("../../../GameFilesEdited/Data/generals.str")
 
     assert generals_str_statusquo.is_file()
